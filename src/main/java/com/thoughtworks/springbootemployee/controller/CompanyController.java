09e04c145069156563e557bff388a61b8e7a23ea
PAUL WONG (INFRA-ISD-OOCLL/HKG) <WONGPA@OOCL.COM>
2020-04-16 00:19:06 +0800

    Employee API

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
@@ -25,11 +26,10 @@ public class CompanyController {
         companies.add(new Company(0, "NIKE", employeesOne));
         companies.add(new Company(1, "ADIDAS", employeesTwo));
 
-        if (page == 0 && pageSize == 0) {
-            return companies;
-        } else {
+        if (page != 0 || pageSize != 0) {
             return companies.stream().filter(company -> company.getCompanyId() >= (page - 1) * pageSize && company.getCompanyId() < page * pageSize).collect(Collectors.toList());
         }
+        return companies;
     }
 
     @GetMapping(path = "/{companyId}")
@@ -73,8 +73,32 @@ public class CompanyController {
                 targetCompany = company;
             }
         }
-        return targetCompany.getEmployees();
+        return Objects.requireNonNull(targetCompany).getEmployees();
     }
+
+//    @PostMapping
+//    public Company addNewCompany(@RequestParam(value = "companyId") int companyId, @RequestParam(value = "companyName") String companyName, @RequestParam(value = "employees") String employees ) {
+//        List<Company> companies = new ArrayList<>();
+//        List<Employee> employeesOne = new ArrayList<>();
+//        List<Employee> employeesTwo = new ArrayList<>();
+//        employeesOne.add(new Employee(0, "Xiaoming", 20, "male", 20000));
+//        employeesOne.add(new Employee(1, "Xiaohong", 19, "female", 20000));
+//        employeesOne.add(new Employee(2, "Xiaozhi", 15, "male", 20000));
+//        employeesTwo.add(new Employee(3, "Xiaomgang", 26, "male", 20000));
+//        employeesTwo.add(new Employee(4, "Xiaoxia", 15, "female", 20000));
+//        companies.add(new Company(0, "NIKE", employeesOne));
+//        companies.add(new Company(1, "ADIDAS", employeesTwo));
+//
+//        Gson gson = new Gson();
+//        NameList nameList = gson.fromJson(data, NameList.class);
+//
+//        List<Name> list = nameList.getList();
+//        List<Employee> newAddEmployees = mapper.readValue(jsonString, StudentList.class);
+//        Company newAddCompany = new Company(companyId, companyName, employees);
+//        companies.add(newAddCompany);
+//        return newAddCompany;
+//    }
+
 }
 
 
--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -12,19 +12,20 @@ import java.util.stream.Collectors;
 public class EmployeesController {
 
     @GetMapping
-    public List<Employee> getAllEmployees(@RequestParam(value="gender",required = false) String gender){
+    public List<Employee> getAllEmployees(@RequestParam(value = "gender", required = false, defaultValue = "Null") String gender, @RequestParam(value = "page", required = false, defaultValue = "0") int page, @RequestParam(value = "pageSize", required = false, defaultValue = "0") int pageSize) {
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee(0, "Xiaoming", 20, "male", 20000));
         employees.add(new Employee(1, "Xiaohong", 19, "female", 20000));
         employees.add(new Employee(2, "Xiaozhi", 15, "male", 20000));
         employees.add(new Employee(3, "Xiaomgang", 26, "male", 20000));
         employees.add(new Employee(4, "Xiaoxia", 15, "female", 20000));
-        if(gender.isEmpty()) {
-            return employees;
-        }
-        else{
+        if (!gender.equals("Null")) {
             return employees.stream().filter(employee -> employee.getGender().equals(gender)).collect(Collectors.toList());
         }
+        if (page != 0 && pageSize != 0) {
+            return employees.stream().filter(employee -> employee.getId() >= (page - 1) * pageSize && employee.getId() < page * pageSize).collect(Collectors.toList());
+        }
+        return employees;
     }
 
     @GetMapping(path = "/{employeeID}")
@@ -43,4 +44,50 @@ public class EmployeesController {
         }
         return targetEmployee;
     }
+
+    @PostMapping
+    public Employee addEmployee(@RequestParam(value = "id") int employeeId, @RequestParam(value = "name") String employeeName, @RequestParam(value = "age") int employeeAge, @RequestParam(value = "gender") String employeeGender, @RequestParam(value = "salary") int employeeSalary) {
+        List<Employee> employees = new ArrayList<>();
+        Employee addEmployee = new Employee(employeeId, employeeName, employeeAge, employeeGender, employeeSalary);
+        employees.add(new Employee(0, "Xiaoming", 20, "male", 20000));
+        employees.add(new Employee(1, "Xiaohong", 19, "female", 20000));
+        employees.add(new Employee(2, "Xiaozhi", 15, "male", 20000));
+        employees.add(new Employee(3, "Xiaomgang", 26, "male", 20000));
+        employees.add(new Employee(4, "Xiaoxia", 15, "female", 20000));
+        employees.add(addEmployee);
+        return addEmployee;
+    }
+
+    @PutMapping(path = "/{employeeId}")
+    public Employee modifyEmployee(@PathVariable("employeeId") int employeeId, @RequestParam(value = "name") String employeeName, @RequestParam(value = "age") int employeeAge, @RequestParam(value = "gender") String employeeGender, @RequestParam(value = "salary") int employeeSalary) {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(0, "Xiaoming", 20, "male", 20000));
+        employees.add(new Employee(1, "Xiaohong", 19, "female", 20000));
+        employees.add(new Employee(2, "Xiaozhi", 15, "male", 20000));
+        employees.add(new Employee(3, "Xiaomgang", 26, "male", 20000));
+        employees.add(new Employee(4, "Xiaoxia", 15, "female", 20000));
+        for (Employee employee : employees) {
+            if (employee.getId() == employeeId) {
+                employee.setName(employeeName);
+                employee.setAge(employeeAge);
+                employee.setGender(employeeGender);
+                employee.setSalary(employeeSalary);
+                return employee;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping(path = "/{employeeId}")
+    public void deleteEmployee(@PathVariable("employeeId") int employeeId) {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(0, "Xiaoming", 20, "male", 20000));
+        employees.add(new Employee(1, "Xiaohong", 19, "female", 20000));
+        employees.add(new Employee(2, "Xiaozhi", 15, "male", 20000));
+        employees.add(new Employee(3, "Xiaomgang", 26, "male", 20000));
+        employees.add(new Employee(4, "Xiaoxia", 15, "female", 20000));
+        employees.removeIf(employee -> employee.getId() == employeeId);
+    }
 }
+
+
